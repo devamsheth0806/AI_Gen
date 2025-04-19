0d110678ae665eaeddab45f76c3a07a17636c572
Your Name <rohit.dadalani@sjsu.edu>
2025-04-18 22:17:07 -0700

    corrected imports

--- crew.py
+++ crew.py
@@ -1,16 +1,28 @@
 from crewai import Crew
 from agents.customer_agent import get_customer_agent
-from agents.k8s_expert import get_k8s_agent
-from agents.vm_expert import get_vm_agent
-from agents.serverless_expert import get_serverless_agent
+from agents.k8s_agent import get_k8s_agent
+from agents.vm_agent import get_vm_agent
+from agents.serverless_agent import get_serverless_agent
 
 from tasks.assess_k8s import assess_k8s_task
 from tasks.assess_vm import assess_vm_task
 from tasks.assess_serverless import assess_serverless_task
-from tasks.summarize_recommendation import summarize_task
+from tasks.summarize import summarize_task
+
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+openai_key = os.getenv("OPENAI_API_KEY")
+
+
+
 
 from openai import OpenAI
-llm = OpenAI(model="gpt-4")  # or use Gemini's wrapper
+llm = OpenAI(api_key=openai_key, model="gpt-4")  
 
 customer = get_customer_agent(llm)
 k8s = get_k8s_agent(llm)
